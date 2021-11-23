d990086fd27e5b46f658ebc99ac57a381c31202e
Henry Joerg <henry.joerg@gmail.com>
2021-11-23 19:07:53 +0100

    logic for comparing input to calc map added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import InputForm from './InputForm';
 
 function App() {
   const [userInput, setUserInput] = useState({
-    straightA: 0,
+    straightA: 115,
     angleA: 0,
     straightB: 0,
     angleB: 0,
@@ -12,9 +12,111 @@ function App() {
     angleC: 0,
   });
 
+  const flattenValue = (input) => (input === 0 ? 0 : 1);
+
+  // eslint-disable-next-line no-unused-vars
+  const [userInputArray, setUserInputArray] = useState([
+    flattenValue(userInput.straightA),
+    flattenValue(userInput.angleB),
+    flattenValue(userInput.straightC),
+    flattenValue(userInput.angleA),
+    flattenValue(userInput.straightB),
+    flattenValue(userInput.angleC),
+  ]);
+
+  // eslint-disable-next-line no-unused-vars
+  const [calcMap, setCalcMap] = useState([]);
+
+  const initCalcMap = [
+    {
+      calcFunction: 'allSides',
+      mappingArrays: [[1, 0, 1, 0, 1, 0]],
+      matchingCount: 0,
+    },
+    {
+      calcFunction: 'twoSidesAngleAttached',
+      mappingArrays: [
+        [0, 0, 1, 0, 1, 1],
+        [1, 1, 0, 0, 1, 0],
+        [1, 0, 1, 1, 0, 0],
+        [1, 0, 0, 1, 1, 0],
+        [1, 0, 1, 0, 0, 1],
+        [0, 1, 1, 0, 1, 0],
+      ],
+      matchingCount: 0,
+    },
+    {
+      calcFunction: 'twoSidesAngleBetween',
+      mappingArrays: [
+        [0, 0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1, 1],
+        [1, 1, 1, 0, 0, 0],
+      ],
+      matchingCount: 0,
+    },
+    {
+      calcFunction: 'oneSideAngleAttachedAngleFloating',
+      mappingArrays: [
+        [0, 0, 1, 1, 0, 1],
+        [0, 1, 0, 0, 1, 1],
+        [1, 1, 0, 1, 0, 0],
+        [0, 1, 0, 1, 1, 0],
+        [1, 0, 0, 1, 0, 1],
+        [0, 1, 1, 0, 0, 1],
+      ],
+      matchingCount: 0,
+    },
+    {
+      calcFunction: 'sideWithTwoAttachedAngles',
+      mappingArrays: [
+        [0, 0, 0, 1, 1, 1],
+        [1, 1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0, 0],
+      ],
+      matchingCount: 0,
+    },
+    {
+      calcFunction: 'twoAngles',
+      mappingArrays: [
+        [0, 0, 0, 1, 0, 1],
+        [0, 1, 0, 0, 0, 1],
+        [0, 1, 0, 1, 0, 0],
+      ],
+      matchingCount: 0,
+    },
+  ];
+
+  const compareInputToCalcMappings = () => {
+    const tmpCalcMap = initCalcMap;
+
+    // loop through all calc maps
+    tmpCalcMap.forEach((actualCalcMap, i) => {
+      const tmpCountArray = [];
+      // loop through all mapping arrays
+      actualCalcMap.mappingArrays.forEach((actualMappingArray) => {
+        let tmpCount = 0;
+        // loop through each value in the mapping array
+        actualMappingArray.forEach((actualMappingArrayValue, j) => {
+          if (userInputArray[j] === actualMappingArrayValue) {
+            tmpCount += 1;
+          }
+        });
+        tmpCountArray.push(tmpCount);
+      });
+      tmpCalcMap[i].matchingCount = Math.max.apply(null, tmpCountArray);
+    });
+    setCalcMap(tmpCalcMap);
+  };
+
   const handleInput = (event) => {
     setUserInput({ ...userInput, [event.target.name]: event.target.value });
   };
+
+  // just for testing
+  useEffect(() => {
+    compareInputToCalcMappings();
+    console.log(calcMap);
+  }, []);
 
   return (
     <div className="App">
